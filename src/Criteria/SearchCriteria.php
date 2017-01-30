7fafef07735a689c028bde4b2933f8ab694e6a94
Patrick Samson <patricksamson236@gmail.com>
2017-01-30 18:05:48 -0500

    Allow searching over multiple columns

--- src/Criteria/SearchCriteria.php
+++ src/Criteria/SearchCriteria.php
@@ -7,16 +7,25 @@ use Prettus\Repository\Contracts\RepositoryInterface;
 
 class SearchCriteria implements CriteriaInterface
 {
-    private $field;
+    private $fields;
     private $search;
 
-    public function __construct($field, $search) {
-        $this->field = $field;
-        $this->search = '%' . $search . '%';
+    public function __construct($fields, $search)
+    {
+        $this->fields = $fields;
+        $this->search = '%'.$search.'%';
     }
 
     public function apply($model, RepositoryInterface $repository)
     {
-        return $model->where($this->field, 'like', $this->search);
+        if (is_array($this->fields)) {
+            return $model->where(function ($query) {
+                foreach ($this->fields as $field) {
+                    $query->orWhere($field, 'like', $this->search);
+                }
+            });
+        }
+
+        return $model->where($this->fields, 'like', $this->search);
     }
 }
