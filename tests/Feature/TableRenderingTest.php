6182341c72cc62ebfc11180f2d275b350e8df323
Patrick Samson <1416027+patricksamson@users.noreply.github.com>
2022-04-04 14:16:41 -0400

    Update TableRenderingTest.php

--- tests/Feature/TableRenderingTest.php
+++ tests/Feature/TableRenderingTest.php
@@ -18,18 +18,21 @@ class TableRenderingTest extends Orchestra\Testbench\TestCase
      */
     protected function getEnvironmentSetUp($app)
     {
+        $app['config']->set('tableview.default-table-view', 'tableview::tableview');
         $app['router']->get('dummyTable', function () {
             return $this->dummyTable->render();
         });
     }
 
     /** @test */
-    /*public function test_it_renders_table()
+    public function test_it_renders_table()
     {
-        $response = $this->get('dummyTable');
+        //$response = $this->get('dummyTable');
 
-        $response->assertSee('id="'.$this->dummyTable->getHtmlId().'"');
-    }*/
+        //$response->assertSee('id="'.$this->dummyTable->getHtmlId().'"');
+        
+        $this->assertStringContainsString('id="'.$this->dummyTable->getHtmlId().'"', $this->dummyTable->render());
+    }
 }
 
 class DummyTable extends \Lykegenes\TableView\AbstractTableView
